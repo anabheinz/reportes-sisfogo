66b63e80000ae15e3cedef040257b6752d96c2db
Ana Beatriz Heinzelmann <anabheinz@gmail.com>
2025-07-07 11:14:16 -0300

    Configura GitHub Actions para exportação automática

--- export.py
+++ export.py
@@ -1,10 +1,14 @@
-from sqlalchemy import create_engine
+import os
 import pandas as pd
+from sqlalchemy import create_engine
 
-secrets = st.secrets["postgres"]
-engine = create_engine(
-    f"postgresql://{secrets.user}:{secrets.password}@{secrets.host}:{secrets.port}/{secrets.database}"
-)
+user = os.environ["PG_USER"]
+password = os.environ["PG_PASSWORD"]
+host = os.environ["PG_HOST"]
+port = os.environ["PG_PORT"]
+database = os.environ["PG_DATABASE"]
 
-df = pd.read_sql("SELECT * FROM editor02.combates_2024", engine)
+engine = create_engine(f"postgresql://{user}:{password}@{host}:{port}/{database}")
+
+df = pd.read_sql("SELECT * FROM editor02.combates_2024", con=engine)
 df.to_csv("dados/combates_2024.csv", index=False)
